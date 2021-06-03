1adf6e88cfaf5e65ad0466a7e0365321d20ea12f
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-06-03 22:43:21 +0200

    Initial Commit

---
+++ FR_AA_05 - Wycieraczka/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FR_AA_05 - Wycieraczka
+//
+//  Created by Kasia on 03/06/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
